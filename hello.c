10abf0b956817f0e4fae2572ddc212ff684f1afb
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-02-14 07:21:19 +0100

    variables

--- hello.c
+++ hello.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 
-int main() {
-    // my first program in C
-    printf("Hello, World! \n");
+// int main(){
+//     printf("I wrote this code my self \n");
+//     printf("I just want to see if it will go to the next line");
 
+//     return 0;
+// }
+
+
+int myNumber;
+
+int main(){
+    myNumber = 22;
+    printf("%d", myNumber);
+    
     return 0;
 }
 
